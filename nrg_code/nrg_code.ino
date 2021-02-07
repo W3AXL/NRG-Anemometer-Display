1be1198605aca132b8de98810825b390c9669aeb
W3AXL <W3AXL@users.noreply.github.com>
2021-02-07 16:15:47 -0600

    completed initial work on SW using FreqMeasure library. Required rewiring of board to pin PC7, will update in future SCH revision

--- nrg_code/nrg_code.ino
+++ nrg_code/nrg_code.ino
@@ -3,84 +3,45 @@
 
 #include "fan.c" // fan bitmap
 
-#define DEBUGMODE
+#include "src/FreqMeasure/FreqMeasure.h"
+
+//#define DEBUGMODE
 
 // compile time information
 const char compile_date[] = __DATE__;
 // version
-const char swVersion[] = "0.2";
+const char swVersion[] = "1.0";
 
-#define PULSEPIN A5
+// Frequency Measurement Limits
+#define MINFREQ 1     // approx 1.129 m/s or 2.5 mph
+#define MAXFREQ 100   // approx 101.1 m/s or 226.2 mph
 
-#define THRESH 128
+// Max speed reset timer in ms
+#define RESETTIMEOUT (30*1000)
 
-#define AVGSMP 5 // current value moving average samples
-
-#define MAXPERIOD 5000000 // max valid period in uS
-#define MINPERIOD 5000    // min valid period in uS
-
-#define MINSPEED 2 // minimum valid speed in mph
-#define MAXSPEED 250 // max valid speed in mph
-
-#define OUTLIER_MULTIPLIER 5 // the current running average multipled and divided by this term defines the bounds of the outlier detection algorithm
-
-#define INTERVAL 1000
-
-#define SCREENINTERVAL 375
-
-// calibration data
+// calibration data from NRG sensor
 #define SLOPE 0.759
 #define OFFSET 0.37
 
-// timing measurement variables
-bool trig = false;
-unsigned long prevTime = 0;
-unsigned long newTime = 0;
+// conversion factor for m/s to mph
+#define MPHFACTOR 2.23694
 
-// real-time moving average storage
-unsigned long avgArray[AVGSMP] = {MAXPERIOD};
-int avgIndex = 0;
-unsigned long lastAvgTime = 0;
+// storage for max freq
+float maxFreq;
 
-// storage for max speed (min period) (start at max period)
-unsigned long lowPeriod = MAXPERIOD;
-
-// screen refresh timer
-unsigned long lastScreen = 0;
+// storage for max speed in mph
+unsigned int maxSpeed;
 
 // fan animation counter
 unsigned char fanCount = 0;
 
+// screen update timer
+unsigned long lastUpdate = 0;
+
+// max speed reset vars
+unsigned long lastMax = 0;
+
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
-
-// initialize the average array with all maxperiods
-void initAvg() {
-  for (int i=0;i<AVGSMP;i++) {
-    avgArray[i] = MAXPERIOD;
-  }
-}
-
-unsigned long getAvg() {
-  unsigned long avgTotal = 0;
-  for (int i=0;i<AVGSMP;i++) {
-    avgTotal += avgArray[i];
-  }
-  return avgTotal / AVGSMP;
-}
-
-void appendAvg(unsigned long period) {
-  // get the average up to this point
-  unsigned long oldAvg = getAvg();
-  // check that the new period is within the outlier detection bounds
-  if ( (oldAvg / OUTLIER_MULTIPLIER) < period < (oldAvg * OUTLIER_MULTIPLIER) ) {
-    avgArray[avgIndex] = period;
-    if (avgIndex >= 3) {
-      avgIndex = 0;
-    } else {
-      avgIndex++;
-    }
-  }
-}
 
 void splashScreen() {
   u8g2.clearBuffer();
@@ -105,40 +66,23 @@ void splashScreen() {
   delay(1000);
 }
 
-float periodToSpeed(unsigned long period) {
-  // divide by zero protection
-  if (period < 1) {period = 1;}
-  // convert uS to frequency in Hz
-  float freq = 1 / (float(period) / 1000000);
-  // use parameters to get m/s
-  float mps = (0.759 * float(freq)) + 0.37;
-  // convert to mph
-  float mph = mps * 2.23694;
-  if (mph < MINSPEED) { mph = 0; }
-  if (mph > MAXSPEED) { mph = MAXSPEED; }
-  return mph;
-}
-
-void debugScreen() {
+void debugScreen(float curFreq) {
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_9x15_tr);
   u8g2.setCursor(4,12);
-  u8g2.print("Cur Period:");
+  u8g2.print("Cur Freq:");
   u8g2.setCursor(16,24);
-  u8g2.print(String(getAvg()));
+  u8g2.print(String(curFreq));
   u8g2.setCursor(4,36);
-  u8g2.print("Min Period:");
+  u8g2.print("Max Freq:");
   u8g2.setCursor(16,48);
-  u8g2.print(String(lowPeriod));
+  u8g2.print(String(maxFreq));
   u8g2.setCursor(4,60);
   u8g2.print("DEBUG MODE");
   u8g2.sendBuffer();
 }
 
-void updateScreen() {
-  // calculate speeds from periods
-  float curMph = periodToSpeed(getAvg());
-  float maxMph = periodToSpeed(lowPeriod);
+void updateScreen(int curMph, int maxMph) {
   // clear screen
   u8g2.clearBuffer();
   // print current speed, with extra sauce for right-justification
@@ -159,14 +103,15 @@ void updateScreen() {
   u8g2.setFont(u8g2_font_logisoso20_tr);
   u8g2.setCursor(88,62);
   u8g2.print(String(int(maxMph)));
-  // fan bitmap animation plays if we're moving
-  if (curMph > MINSPEED) {
+  // increment the fan animation on screen update if our speed is not 0
+  if (curMph > 0) {
     if (fanCount > 2) {
       fanCount = 0;
     } else {
       fanCount++;
     }
   }
+  // display the proper fan graphic
   switch (fanCount) {
     case 0:
       u8g2.drawXBM(90,6,fan_width,fan_height,fan1_bits);
@@ -186,64 +131,46 @@ void updateScreen() {
 }
 
 void setup() {
-  // initialize average avgArrray
-  //initAvg();
-  //pinmode 
-  //pinMode(PULSEPIN, INPUT_PULLUP);
+  // initialize frequency measurement
+  FreqMeasure.begin();
   // display setup
   u8g2.begin();
   u8g2.clear();
   u8g2.setBusClock(400000);
-  #ifdef DEBUGMODE
-    debugScreen();
-  #else
+  #ifndef DEBUGMODE
     splashScreen();
-    updateScreen();
   #endif
 }
 
 void loop() {
-  // read the pin
-  int pinRdg = analogRead(PULSEPIN);
-  // it's high if it's above the threshold
-  bool pin = (pinRdg > THRESH);
-  // now logic
-  if (pin && !trig) {
-    // if we're high and we haven't triggered yet, start a measurement
-    trig = true;
-    // store the previous time and record the new one
-    prevTime = newTime;
-    newTime = micros();
-    // calculate that period and check if it's out of range
-    unsigned long period = newTime - prevTime;
-    if (period > MAXPERIOD) { period = MAXPERIOD; }
-    // add it to the current averages
-    appendAvg(period);
-    lastAvgTime = millis();
-    // check if it's a new max low
-    if (getAvg() < lowPeriod) {lowPeriod = getAvg();}
-  } else if (!pin && trig) {
-    // if we were triggered but are now low, reset everything
-    trig = false;
-    // if we've waited longer than the max period, add a max period to the average
-    if (micros() - newTime > MAXPERIOD) {
-      lastAvgTime = millis();
-      appendAvg(MAXPERIOD);
+  // update the screen if we have a new measurement available
+  if (FreqMeasure.available()) {
+    // get the latest measurement
+    float freq = FreqMeasure.countToFrequency(FreqMeasure.read());
+    // convert to mph from freq
+    float rawSpeed = ((freq * SLOPE) + OFFSET) * MPHFACTOR;
+    // round to nearest int by adding 0.5 before casting
+    rawSpeed = rawSpeed + 0.5 - (rawSpeed<0);
+    unsigned int speed = int(rawSpeed);
+    // check if that's a new max
+    if (speed > maxSpeed) { 
+      maxSpeed = speed; 
+      maxFreq = freq;
+      lastMax = millis();
+    } else if (millis() - lastMax > RESETTIMEOUT) {
+      maxSpeed = speed;
+      maxFreq = freq;
     }
-  } else if (!pin && !trig) {
-    // if nothing is happening but we're past the max timeout, add another 0 to the moving average
-    if (millis() - lastAvgTime > INTERVAL) {
-      lastAvgTime = millis();
-      appendAvg(MAXPERIOD);
-    }
-  }
-  // screen update timer
-  if (millis() - lastScreen > SCREENINTERVAL) {
-    lastScreen = millis();
+    // update the screen
     #ifdef DEBUGMODE
-      debugScreen();
+      debugScreen(freq);
     #else
-      updateScreen();
+      updateScreen(speed, maxSpeed);
     #endif
+    // record the screen update time
+    lastUpdate = millis();
+  // if we haven't gotten a frequency update in longer than the minimum measurement period, update the screen
+  } else if (millis() - lastUpdate > (1000/MINFREQ)) {
+    updateScreen(0, maxSpeed);
   }
 }
